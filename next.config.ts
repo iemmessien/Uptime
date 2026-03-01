20197f424e24d99cf8533e9eefbcb370a0ff0dc6
Emmanuel <eessien.10@protonmail.com>
2026-03-02 00:36:49 +0100

    added eslint ignore to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/uptime",
+   basePath: "/uptime",
+
+   eslint: {
+      ignoreDuringBuilds: true,  // ← Add this
+   },
 };
 
 
